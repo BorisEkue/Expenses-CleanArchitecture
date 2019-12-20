299da692fbeb2aea9282f6d97807fb5220c3def7
EKUE-HETTAH Kuévi Boris Claude Marie <bekue@moov.tg>
2019-12-20 19:34:22 +0000

    Create repositories

---
+++ Expenses/Domain/Repositories/IExpenseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Expenses.Domain.Entities;
+
+namespace Expenses.Domain.Repositories
+{
+     interface IExpenseRepository
+    {
+        void Create(Expense expense);
+    }
+}
